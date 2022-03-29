cf7879abaac1d953e0da3fad6b426faf96f00636
yuns <yuns.xie@qq.com>
2022-03-29 23:38:38 +0800

    UI: better header title style

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -23,11 +23,11 @@ const LayoutWrapper = ({ children }: Props) => {
           <div>
             <Link href='/' aria-label={siteMetadata.headerTitle}>
               <div className='flex items-center justify-between'>
-                <div className='mr-3'>
+                <div className='flex items-center sm:mr-3'>
                   <Logo />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className='hidden h-6 text-2xl font-semibold sm:block'>
+                  <div className='hidden text-2xl font-semibold sm:block'>
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
