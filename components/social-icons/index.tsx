bb4cd77f6e863057a4f621c6935ec3519a6f6d19
yuns <yuns.xie@qq.com>
2022-04-17 18:43:03 +0800

    fix: icon size

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -1,5 +1,6 @@
 import Facebook from './facebook.svg'
 import Github from './github.svg'
+import Juejin from './juejin.svg'
 import Linkedin from './linkedin.svg'
 import Mail from './mail.svg'
 import Twitter from './twitter.svg'
@@ -10,13 +11,21 @@ import Youtube from './youtube.svg'
 const components = {
   mail: Mail,
   github: Github,
+  juejin: Juejin,
   facebook: Facebook,
   youtube: Youtube,
   linkedin: Linkedin,
   twitter: Twitter,
 }
 
-const SocialIcon = ({ kind, href, size = 8 }) => {
+export interface SocialIconProps {
+  kind: keyof typeof components
+  href?: string
+  /** unit: rem */
+  size?: number
+}
+
+const SocialIcon = ({ kind, href, size = 2 }: SocialIconProps) => {
   if (
     !href ||
     (kind === 'mail' &&
@@ -24,7 +33,9 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
   )
     return null
 
-  const SocialSvg = components[kind]
+  const SocialSvg = components[kind] as React.ComponentType<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >
 
   return (
     <a
@@ -35,7 +46,11 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
     >
       <span className='sr-only'>{kind}</span>
       <SocialSvg
-        className={`fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-${size} w-${size}`}
+        className={`fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400`}
+        style={{
+          width: `${size}rem`,
+          height: `${size}rem`,
+        }}
       />
     </a>
   )
